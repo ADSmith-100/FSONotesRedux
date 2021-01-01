a2c081cafc38ad77f0392cb01f42868b129d94a1
Adam Smith <coloradoasmith@gmail.com>
2020-12-31 21:47:35 -0700

    refactor using connect instead of hooks api

--- src/components/NewNote.js
+++ src/components/NewNote.js
@@ -1,23 +1,47 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+// import { useDispatch } from "react-redux";
+import { connect } from "react-redux";
 import { createNote } from "../reducers/noteReducer";
 
-const NewNote = () => {
-  const dispatch = useDispatch();
+const NewNote = (props) => {
+  console.log(createNote);
+  console.log(props.createNote);
 
   const addNote = async (event) => {
     event.preventDefault();
     const content = event.target.note.value;
     event.target.note.value = "";
-    dispatch(createNote(content));
+    props.createNote(content);
   };
 
   return (
     <form onSubmit={addNote}>
       <input name="note" />
-      <button type="submit">save</button>
+      <button type="submit">add</button>
     </form>
   );
 };
 
-export default NewNote;
+export default connect(null, { createNote })(NewNote);
+
+// const NewNote = () => {
+//   const dispatch = useDispatch();
+
+//   const addNote = async (event) => {
+//     event.preventDefault();
+//     const content = event.target.note.value;
+//     event.target.note.value = "";
+//     dispatch(createNote(content));
+//   };
+
+//   return (
+//     <form onSubmit={addNote}>
+//       <input name="note" />
+//       <button type="submit">save</button>
+//     </form>
+//   );
+// };
+
+// export default NewNote;
+
+//modified to show old way of using connect instead of hooks-api...
